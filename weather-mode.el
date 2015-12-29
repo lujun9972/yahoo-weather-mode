69f5e4940531fce7ac21b1d2b6da07ce51b38b6b
lujun9972 <lujun9972@gmail.com>
2015-12-29 23:31:19 +0800

    add package header

--- weather-mode.el
+++ weather-mode.el
@@ -1,3 +1,35 @@
+;;; weather.el --- Displays your weather information in mode-line  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2004-2015 Free Software Foundation, Inc.
+
+;; Author: DarkSun <lujun9972@gmail.com>
+;; URL: https://github.com/lujun9972/weather-mode
+;; Package-Requires: ((emacs "24"))
+;; Keywords: weather, mode-line
+;; Created: 2015-12-28
+;; Version: 0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; customize the weather-location and then 
+;;
+;;   M-x weather-mode
+
+;;; Code:
+
 (require 'url)
 (require 'json)
 
